17c479cd1f51146c291b20529c999f2e2ae2d098
Jamie Phillips <jamie.phillips@suse.com>
2022-06-27 13:08:25 -0400

    Basics are in place for wiring up the service.

--- src/Acetylene/Program.cs
+++ src/Acetylene/Program.cs
@@ -1,3 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Acetylene.Service;
+using Serilog;
+using Topshelf;
 
-Console.WriteLine("Hello, World!");
+namespace Acetylene; 
+
+public class Program {
+    public static void Main() {
+        var ts = HostFactory.Run(x => {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .WriteTo.Console()
+                .CreateLogger();
+            x.UseSerilog();
+
+            x.Service<AcetyleneService>(s => {
+                s.ConstructUsing(name => new AcetyleneService());
+                s.WhenStarted(a => a.Start());
+                s.WhenStopped(a => a.Stop());
+            });
+            
+            // Come back to this
+            x.SetDescription("Ignition for Windows!");
+            x.SetDisplayName("Acetylene");
+            x.SetServiceName("Acetylene");
+            
+            x.RunAsLocalSystem();
+            
+            x.SetStartTimeout(TimeSpan.FromSeconds(30));
+            x.SetStopTimeout(TimeSpan.FromSeconds(30));
+
+            x.EnableServiceRecovery(r =>
+            {
+                r.RestartService(3);
+                r.SetResetPeriod(2);
+            });
+            
+
+            x.OnException((exception) =>
+            {
+                Console.WriteLine("Exception thrown - " + exception.Message);
+            });
+        });
+
+        var exitCode = (int)Convert.ChangeType(ts, ts.GetTypeCode());
+        Environment.ExitCode = exitCode;
+    }
+}
